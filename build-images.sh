191d6e76c161826996ec4d4d5093e3898fea8f59
xyzroller <xyzroller@rollyourown.xyz>
2021-10-16 15:18:41 +0200

    Added build-images script

--- build-images.sh
+++ build-images.sh
@@ -2,8 +2,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Add additional help for software package versions defined below
-# Option "-g grav_version" as an example
 helpMessage()
 {
    echo "build-images.sh: Use packer to build images for deployment"
@@ -13,7 +11,6 @@ helpMessage()
    echo "Flags:"
    echo -e "-m \t\t\t(Optional) Flag to also build images for modules"
    echo -e "-n hostname \t\t(Mandatory) Name of the host for which to build images"
-   echo -e "-g grav_version \t(Optional) Override default grav version to use for the webserver image, e.g. 1.7.17 (default)"
    echo -e "-v version \t\t(Mandatory) Version stamp to apply to images, e.g. 20210101-1"
    echo -e "-h \t\t\tPrint this help message"
    echo ""
@@ -28,33 +25,28 @@ errorMessage()
 }
 
 # Default software package versions
-# Add software package versions here, for example grav
-grav_version='1.7.17'
+element_version='1.9.2'
+synapse_version='1.44.0'
 
 build_modules='false'
 
-# Add additional options for software package versions defined above
-# Option "-g grav_version" as an example
-while getopts mn:g:v:h flag
+while getopts mn:v:h flag
 do
     case "${flag}" in
         m) build_modules='true';;
         n) hostname=${OPTARG};;
-        g) grav_version=${OPTARG};;
         v) version=${OPTARG};;
         h) helpMessage ;;
         ?) errorMessage ;;
     esac
 done
 
-if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$grav_version" ]
+if [ -z "$hostname" ] || [ -z "$version" ]
 then
    errorMessage
 fi
 
-# Common to almost all projects - service proxy module
-# Remove if not needed for this project
-# Build module images if -m flag is present
+# Build ryo-service-proxy module images if -m flag is present
 if [ $build_modules == 'true' ]
 then
    echo "Running build-images script for ryo-service-proxy module on "$hostname""
@@ -65,11 +57,53 @@ else
    echo ""
 fi
 
-# Project-specific image builds here..., for example
-#echo "Building webserver image on "$hostname""
-#echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"grav_version="$grav_version"\" -var \"version="$version"\" "$SCRIPT_DIR"/image-build/webserver.pkr.hcl"
-#echo ""
-#packer build -var "host_id="$hostname"" -var "grav_version="$grav_version"" -var "version="$version"" "$SCRIPT_DIR"/image-build/webserver.pkr.hcl
-#echo ""
+# Build ryo-postgres module images if -m flag is present
+if [ $build_modules == 'true' ]
+then
+   echo "Running build-images script for ryo-postgres module on "$hostname""
+   echo ""
+   "$SCRIPT_DIR"/../ryo-postgres/build-images.sh -n "$hostname" -v "$version"
+else
+   echo "Skipping image build for modules"
+   echo ""
+fi
+
+# Build ryo-coturn module images if -m flag is present
+if [ $build_modules == 'true' ]
+then
+   echo "Running build-images script for ryo-coturn module on "$hostname""
+   echo ""
+   "$SCRIPT_DIR"/../ryo-coturn/build-images.sh -n "$hostname" -v "$version"
+else
+   echo "Skipping image build for modules"
+   echo ""
+fi
+
+# Build ryo-wellknown module images if -m flag is present
+if [ $build_modules == 'true' ]
+then
+   echo "Running build-images script for ryo-wellknown module on "$hostname""
+   echo ""
+   "$SCRIPT_DIR"/../ryo-wellknown/build-images.sh -n "$hostname" -v "$version"
+else
+   echo "Skipping image build for modules"
+   echo ""
+fi
+
+
+# Build project images
+echo "Building element-web image on "$hostname""
+echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"element_version=$element_version\" -var \"version=$version\" "$SCRIPT_DIR"/image-build/element.pkr.hcl"
+echo ""
+packer build -var "host_id="$hostname"" -var "element_version=$element_version" -var "version=$version" "$SCRIPT_DIR"/image-build/element.pkr.hcl
+
+echo "Building matrix-synapse image on "$hostname""
+echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version=$version\" -var \"synapse_version=$synapse_version\" "$SCRIPT_DIR"/image-build/synapse.pkr.hcl"
+echo ""
+packer build -var "host_id="$hostname"" -var "version=$version" -var "synapse_version=$synapse_version" "$SCRIPT_DIR"/image-build/synapse.pkr.hcl
+
+###
+### !!! TODO: Add synapse-admin
+###
 
 echo "Completed"
