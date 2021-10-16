2a929e029c050de542923d48400d1d5e379117c9
xyzroller <xyzroller@rollyourown.xyz>
2021-10-16 15:18:01 +0200

    Added get-modules script

--- get-modules.sh
+++ get-modules.sh
@@ -3,8 +3,7 @@
 echo "Getting project-specific modules from repositories..."
 echo ""
 
-## Common to almost all projects - service proxy module repository
-## Remove if not required
+## Service proxy module
 if [ -d "../ryo-service-proxy" ]
 then
    echo "Module ryo-service-proxy already cloned to this control node"
@@ -14,12 +13,32 @@ else
    git clone https://git.rollyourown.xyz/ryo-projects/ryo-service-proxy ../ryo-service-proxy
 fi
 
-## Project specific submodules
-# if [ -d "../<MODULE_DIRECTORY>" ]
-# then
-#    echo "Module <MODULE_NAME> already cloned to this control node"
-# else
-#    echo "Cloning <MODULE_NAME> repository. Executing 'git clone' for <MODULE_NAME> repository"
-#    #git clone https://github.com/rollyourown-xyz/<MODULE_NAME> ../<MODULE_NAME>
-#    git clone https://git.rollyourown.xyz/ryo-projects/<MODULE_NAME> ../<MODULE_NAME>
-# fi
+# PostgreSQL Database module
+if [ -d "../ryo-postgres" ]
+then
+   echo "Module ryo-postgres already cloned to this control node"
+else
+   echo "Cloning ryo-postgres repository. Executing 'git clone' for ryo-postgres repository"
+   #git clone https://github.com/rollyourown-xyz/ryo-postgres ../ryo-postgres 
+   git clone https://git.rollyourown.xyz/ryo-projects/ryo-postgres ../ryo-postgres 
+fi
+
+## Wellknown Server module
+if [ -d "../ryo-wellknown" ]
+then
+   echo "Module ryo-wellknown already cloned to this control node"
+else
+   echo "Cloning ryo-wellknown repository. Executing 'git clone' for ryo-wellknown repository"
+   #git clone https://github.com/rollyourown-xyz/ryo-wellknown ../ryo-wellknown
+   git clone https://git.rollyourown.xyz/ryo-projects/ryo-wellknown ../ryo-wellknown
+fi
+
+# STUN/TURN Server module
+if [ -d "../ryo-coturn" ]
+then
+   echo "Module ryo-coturn already cloned to this control node"
+else
+   echo "Cloning ryo-coturn repository. Executing 'git clone' for ryo-coturn repository"
+   #git clone https://github.com/rollyourown-xyz/ryo-coturn ../ryo-coturn
+   git clone https://git.rollyourown.xyz/ryo-projects/ryo-coturn ../ryo-coturn
+fi
