d20c441b3cb7e718b06aafbcce11377f79cd8480
xyzroller <xyzroller@rollyourown.xyz>
2022-01-13 17:49:32 +0100

    Consistent mount directory and ingress-proxy names

---
+++ project-deployment-standalone/07-deploy-ingress-proxy-configuration-synapse.tf
@@ -0,0 +1,42 @@
+# Deploy Ingress Proxy configuration for synapse
+################################################
+
+module "deploy-matrix-ingress-proxy-backend-services-for-synapse" {
+
+  depends_on = [ lxd_container.synapse ]
+
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-backend-services"
+
+  non_ssl_backend_services = [ "synapse" ]
+}
+
+module "deploy-matrix-ingress-proxy-acl-configuration-for-synapse" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-matrix-ingress-proxy-backend-services-for-synapse ]
+
+  ingress-proxy_path_only_acls = {
+    path-synapse-admin  = {path = "/_synapse/admin"}
+    path-synapse-client = {path = "/_synapse/client"}
+    path-matrix         = {path = "/_matrix"}
+  }
+}
+
+module "deploy-matrix-ingress-proxy-deny-configuration-for-synapse" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-matrix-ingress-proxy-acl-configuration-for-synapse ]
+
+  ingress-proxy_acl_denys = [ "path-synapse-admin" ]
+}
+
+module "deploy-matrix-ingress-proxy-backend-configuration-for-synapse" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-matrix-ingress-proxy-acl-configuration-for-synapse ]
+
+  ingress-proxy_acl_use-backends = {
+    path-synapse-client = {backend_service = "synapse"},
+    path-matrix         = {backend_service = "synapse"}
+  }
+}
