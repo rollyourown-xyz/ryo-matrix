c6422cec57cf46ada0a00dd34dec3b331082bf11
xyzroller <xyzroller@rollyourown.xyz>
2022-08-16 19:00:04 +0200

    Bump Element to v1.11.3 and Synapse to v1.65.0

--- scripts-project/build-image-project.sh
+++ scripts-project/build-image-project.sh
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Default project software versions
-element_version="1.11.2"
-synapse_version="1.64.0"
+element_version="1.11.3"
+synapse_version="1.65.0"
 synapse_admin_version="0.8.5"
 
 # Script directory
